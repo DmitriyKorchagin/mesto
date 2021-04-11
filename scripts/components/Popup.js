1ac7951cb03b894c3c812b22e94e9890b57b0c92
Dmitriy Korchagin <mrontari@yandex.ru>
2021-04-11 12:37:48 +0300

    рефакторинг ооп перед webpack

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -1,29 +1,46 @@
 export default class Popup {
+
     constructor(popupSelector){
         this._popupElement = document.querySelector(popupSelector);
+        this._closePopup = {
+            esc: this._closePopupWithEscape.bind(this),
+            click: this._closePopupWithClick.bind(this)
+            
+            
+        }
     }
 
 
 //метод открытия
     openPopup(){
         this._popupElement.classList.add('popup_opened');
+        document.addEventListener('key', this._closePopup.esc);
 
     }
 
 //метод закрытия
     closePopup(){
         this._popupElement.classList.remove('popup_opened');
+        document.removeEventListener('key', this._closePopup.esc);
     }
 
 //метод закрытия по esc
-    _handleEscClose(evt) {
+    _closePopupWithEscape(evt) {
         if (evt.key === 'Escape') {
         this.closePopup();
         }
     }
+
+
+    _closePopupWithClick(evt) {
+        if (evt.target.classList.contains('popup__close-button') || evt.target.classList.contains('popup')) {
+            this.closePopup();
+        }
+    }
+
 //слушатели
     setEventListeners(){
-        this._popupElement.addEventListener('click', this._closePopup);
+        this._popupElement.addEventListener('click', this._closePopup.click);
 
     }
 }
